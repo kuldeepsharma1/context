bf0f3178b4d29f08da49c0e245c33e9cbf88a571
kuldeepsharma1 <hytek112@gmail.com>
2025-02-16 15:38:48 +0530

    intermediate context exaplin

--- src/main.tsx
+++ src/main.tsx
@@ -1,33 +1,9 @@
-import { createContext, StrictMode, FC, useState, ReactNode, useContext } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App';
-interface ThemeContextType {
-  theme: "light" | "dark";
-  toggleTheme: () => void;
-}
-
-const ThemeContext = createContext<ThemeContextType | null>(null);
-
-const ThemeProvider: FC<{ children: ReactNode }> = ({ children }) => {
-  const [theme, setTheme] = useState<"light" | "dark">("light");
-  const toggleTheme = () => {
-    setTheme((prev) => (prev === "light" ? "dark" : "light"));
-  };
-
-  return (
-    <ThemeContext.Provider value={{ theme, toggleTheme }}>
-      {children}
-    </ThemeContext.Provider>
-  )
-}
-
-export const useTheme = () => {
-  const context = useContext(ThemeContext)
-  if (!context) {
-    throw new Error("useTheme must be used within a ThemeProvider");
-  }
-  return context;
-}
+// import App from './App';
+// import ThemeProvider from './contexts/ThemeContext';
+import Profile from './Profile';
+import AuthProvider from './contexts/AuthContext';
 
 
 
@@ -42,10 +18,13 @@ createRoot(document.getElementById('root')!).render(
 3. Consumer -> Consumes context value
 4 useContext -> hook to access Context value 
 */}
-    <ThemeProvider>
+    {/* <ThemeProvider>
       <App />
-    </ThemeProvider>
+    </ThemeProvider> */}
 
+    <AuthProvider>
+      <Profile />
+    </AuthProvider>
 
   </StrictMode>,
 )
